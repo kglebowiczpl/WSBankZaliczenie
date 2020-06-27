b17745b03b11e5f937f9739e08a0d363f3ce09bf
JFraczkowski <67468860+JFraczkowski@users.noreply.github.com>
2020-06-27 20:45:32 +0200

    Add files via upload
    Połączenie z baza danych i zarządzanie użytkownikami

---
+++ WSBank/WSDB.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace WSBank
+{
+    class WSDB
+    {
+        private MySqlConnection conn;
+        private string connectionString;
+        public WSDB(string login,string password,string host,string dbname)
+        {
+            this.connectionString = "server="+host+";database="+dbname+";uid="+login+";pwd="+password+";";
+        }
+        public bool connect()
+        {
+            conn = new MySqlConnection(connectionString);
+            try
+            {
+                conn.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public bool close()
+        {
+            try
+            {
+                conn.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+        public MySqlConnection getConnection()
+        {
+            return conn;
+        }
+    }
+}
---
+++ WSBank/WSUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSBank
+{
+    class WSUser
+    {
+        private string userid;
+        private string imie;
+        private string nazwisko;
+        private int data_otwarcia;
+        private int saldo;
+
+        public WSUser(string userid,string imie,string nazwisko,int data_otwarcia,int saldo)
+        {
+            this.userid = userid;
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.data_otwarcia = data_otwarcia;
+            this.saldo = saldo;
+        }
+        public string getFullName()
+        {
+            return imie + " " + nazwisko;
+        }
+        public string getUserId()
+        {
+            return userid;
+        }
+
+    }
+}
